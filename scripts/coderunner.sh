886b864966d88f147e543c2a7cc4df950f078bcb
Naveen <wasabi.naveen@gmail.com>
2026-01-18 00:22:07 +0100

    feat : seq optim (58.0 fps -> 212.5 fps)

--- scripts/coderunner.sh
+++ scripts/coderunner.sh
@@ -31,7 +31,9 @@ case $choice in
         # run the code. //ici
         cd build
         # ./bin/murb -n 1000 -i 1000 -v
-        ./bin/murb -n 1000 -i 1000 -v --nv --im cpu+naive
+        #here
+        # ./bin/murb -n 1000 -i 1000 -v --nv --im cpu+naive
+        ./bin/murb -n 1000 -i 1000 -v --nv --im cpu+optim
 
         # final test.
         # ./bin/murb -n 30000 -i 10000 -v --nv --im [ImplTag]
